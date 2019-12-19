33b6022f76a0d7bfc7d5a54d7711a54f8c7a3541
Vladimir Malygin <vldmr.one@gmail.com>
2019-12-20 00:35:05 +0300

    feat: implement query search and 404 page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
@@ -8,9 +9,11 @@ import './style/index.scss';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <Router>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </Router>
   </Provider>
   , document.getElementById('root')
 );
