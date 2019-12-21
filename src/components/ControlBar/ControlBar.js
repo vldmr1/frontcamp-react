add9420538189baacb0e247dd850a255c2451f0e
Vladimir Malygin <vldmr.one@gmail.com>
2019-12-21 15:55:12 +0300

    dev: add unit tests for reducers and actions

--- src/components/ControlBar/ControlBar.js
+++ src/components/ControlBar/ControlBar.js
@@ -40,7 +40,6 @@ const ControlBar = ({ changeSortType, filmsCount, sortBy, currentMovie, movies, 
       <Route exact path="/(|search)/">
         <ToggleSwitch
           toggleHandler={(event) => {
-            console.log(sortBy);
             changeSortType(event.target.value.toLowerCase());
             if (searchQuery) fetchMovies(searchQuery);
           }}
